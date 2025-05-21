228c3118694ca0dcb14bb529728eb95d17fe64f5
dedevquaresma <andrequaresma25@gmail.com>
2025-05-20 22:21:07 -0300

    primeira versão

---
+++ src/test/java/org/base/GreetingResourceITTest.java
@@ -0,0 +1,10 @@
+//package org.base;
+//
+//import io.quarkus.test.junit.QuarkusIntegrationTest;
+//import io.quarkus.test.junit.QuarkusTest;
+//
+////@QuarkusIntegrationTest
+//@QuarkusTest
+//class GreetingResourceITTest extends GreetingResourceTest {
+//    // Execute the same tests but in packaged mode.
+//}
